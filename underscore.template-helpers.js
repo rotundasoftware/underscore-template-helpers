5063ab0816b4f2920513969dc06f80fb3758284c
Nate Steffan <nate.steffan@dynamichorizons.net>
2012-12-15 20:30:22 -0600

    Add local template - template helper capability and fixed small issue when dealing with undefined data.
    Signed-off-by: Nate Steffan <nate.steffan@dynamichorizons.net>

--- underscore.template-helpers.js
+++ underscore.template-helpers.js
@@ -8,6 +8,15 @@
 		},
 
 		template : function(text, data, settings) {
+			// Create instance of template helpers for just this template
+			var localTemplateHelpers = _.clone(templateHelpers);
+		
+			// In case of template helpers defined for this instance only, check and extend
+			if( settings && settings['templateHelpers'] ) 
+			{
+				_.extend( localTemplateHelpers, settings['templateHelpers'] );
+			}
+		
 			// replace the built in _.template function with one that supports the addTemplateHelpers
 			// function above. Basically the combo of the addTemplateHelpers function and this new
 			// template function allows us to mix in global "helpers" to the data objects passed
@@ -19,15 +28,21 @@
 				// if data is supplied, the original _.template function just returns the raw value of the
 				// render function (the final rentered html/text). So in this case we just extend
 				// the data param with our templateHelpers and return raw value as well.
-
-				_.defaults( data, templateHelpers ); // extend data with our helper functions
+				
+				_.defaults( data, localTemplateHelpers ); // extend data with our helper functions
 				return originalUnderscoreTemplateFunction.apply( this, arguments ); // pass the buck to the original _.template function
 			}
 			
 			var template = originalUnderscoreTemplateFunction.apply( this, arguments );
 
 			var wrappedTemplate = function( data ) {
-				_.defaults( data, templateHelpers );
+				// Create Empty Object In Case Data is Undefined
+				if( ! data ) 
+				{
+					data = {};
+				}
+				
+				_.defaults( data, localTemplateHelpers );
 				return template.apply( this, arguments );
 			};
 
